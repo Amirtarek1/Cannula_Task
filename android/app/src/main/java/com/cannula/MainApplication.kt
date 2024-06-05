a4cb2ce94f461aac1212a71a100ebd132c262bd3
Amirtarek1 <amir.tarek8880@gmail.com>
2024-06-05 07:56:36 +0300

    upload new task name cannula for Depax company

--- android/app/src/main/java/com/cannula/MainApplication.kt
+++ android/app/src/main/java/com/cannula/MainApplication.kt
@@ -10,6 +10,7 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
+import com.facebook.react.modules.i18nmanager.I18nUtil
 
 class MainApplication : Application(), ReactApplication {
 
@@ -31,13 +32,18 @@ class MainApplication : Application(), ReactApplication {
 
   override val reactHost: ReactHost
     get() = getDefaultReactHost(applicationContext, reactNativeHost)
-
-  override fun onCreate() {
+override fun onCreate() {
     super.onCreate()
     SoLoader.init(this, false)
+
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      load()
+        // If you opted-in for the New Architecture, we load the native entry point for this app.
+        load()
     }
-  }
+
+    val sharedI18nUtilInstance = I18nUtil.getInstance()
+    sharedI18nUtilInstance.forceRTL(this, true)
+    sharedI18nUtilInstance.allowRTL(this, true)
+}
+  
 }
